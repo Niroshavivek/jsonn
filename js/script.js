16bb11b6498e062ec70ea786a1859b131dcd85f2
Niroshavivek <niroshaisteen@gmail.com>
2017-04-04 00:34:37 +0530

    jss

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+	var a="i irosha";
+	var b="vivek";
+var v3={"companies":[],"com":[]};
+v3.companies[0]="oo";
+v3.companies[1]="vi";
+v3.companies[2]="pp";
+for(var i=0;i<v3.companies.length;i++)
+{
+$("ul").append("<li>"+v3.companies[i]+"</li>");
+}
+})
